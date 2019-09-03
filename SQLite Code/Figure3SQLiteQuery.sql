6af8a22d67551b3ace02a093fa414df0c58160f6
Sonia Balani <39965743+soniabalani@users.noreply.github.com>
2019-09-03 01:46:11 -0400

    Delete Figure3SQLiteQuery.sql

--- SQLite Code/Figure3SQLiteQuery.sql
+++
@@ -1,6 +0,0 @@
-SELECT tesla_tweets.Day AS Date,(Close-Open) AS "Change in Stock Price", 
-Relevant_Tweets AS "Tweets Relevant to Tesla", Irrelevant_Tweets AS "Tweets Not Relevant to Tesla",
-Total_Tweets AS "Total Tweets", ROUND((0.3*ABS(Close-Open))+(0.7*Total_Tweets),3) AS Score
-FROM tesla_tweets INNER JOIN tesla_stock ON tesla_tweets.Day = tesla_stock.Date 
-WHERE Total_Tweets > 3 and ABS((Close-Open)) > 0.04
-ORDER BY Score DESC
