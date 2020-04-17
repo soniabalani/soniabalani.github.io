38eeb13317b36e6169aac94fa5e3e6ae8a99fe9d
Sonia Balani <39965743+soniabalani@users.noreply.github.com>
2020-04-17 02:47:00 -0400

    Update Figure6SQLQuery.sql

--- Data/Figure6SQLQuery.sql
+++ Data/Figure6SQLQuery.sql
@@ -1,4 +1,4 @@
-SELECT tesla_sentiment.Date, (Close-Open) AS "Change in Stock Price",Tweets_Tesla AS "Number of Tweets about Tesla", 
+SELECT tesla_sentiment.Date, ROUND((Close-Open),2) AS "Change in Stock Price", Tweets_Tesla AS "Number of Tweets about Tesla", 
 Positive, Negative, Neutral, ROUND((0.3*ABS(Close-Open))+(0.7*Total_Tweets),2) AS Score   
 FROM tesla_sentiment INNER JOIN tesla_stock ON tesla_sentiment.Date = tesla_stock.Date 
 INNER JOIN tesla_tweets ON tesla_stock.Date = tesla_tweets.Day
