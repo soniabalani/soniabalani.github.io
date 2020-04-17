813d586747adb48a9b3c55c978965835f7357fb9
Sonia Balani <39965743+soniabalani@users.noreply.github.com>
2020-04-17 02:47:23 -0400

    Update Figure4SQLQuery.sql

--- Data/Figure4SQLQuery.sql
+++ Data/Figure4SQLQuery.sql
@@ -1,4 +1,4 @@
-SELECT tesla_tweets.Day AS Date,(Close-Open) AS "Change in Stock Price", 
+SELECT tesla_tweets.Day AS Date, ROUND((Close-Open),2) AS "Change in Stock Price", 
 Relevant_Tweets AS "Tweets Relevant to Tesla", Irrelevant_Tweets AS "Tweets Not Relevant to Tesla",
 Total_Tweets AS "Total Tweets", ROUND((0.3*ABS(Close-Open))+(0.7*Total_Tweets),2) AS Score
 FROM tesla_tweets INNER JOIN tesla_stock ON tesla_tweets.Day = tesla_stock.Date 
